255dabff35466485461b2e5cd46777a9da823556
viniciusOP <viniciusopassos@hotmail.com>
2022-11-16 16:39:48 -0300

    feat: query 9

---
+++ SQL_query9.sql
@@ -0,0 +1,7 @@
+-- 9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+
+SELECT * FROM Atores
+
+WHERE Genero = 'F'
+
+ORDER BY PrimeiroNome
